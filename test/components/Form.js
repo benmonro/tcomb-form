4c51856b8858e5c1ff8506ffd51a4c9052c61d82
Monro, Ben <Ben.Monro@concur.com>
2015-09-11 11:12:18 -0700

    failing test

--- test/components/Form.js
+++ test/components/Form.js
@@ -6,11 +6,7 @@ var Form = t.form.Form;
 
 
 var path = require('path');
-var fs = require('fs');
-var jsonString = fs.readFileSync(path.join(__dirname, '../sample/data.json'), 'utf8');
-
-var sample = JSON.parse(jsonString); // require('../../samples/simpleForm');
-var transformer = {
+var fs = require('fs');var transformer = {
     format: function (value) {
         return t.Str.is(value) ? value : value === true ? '1' : '0';
     },
@@ -19,20 +15,27 @@ var transformer = {
     }
 };
 
-tape('Form', function(tape) {
-    console.log('testttt')
-    tape.test('value', function(tape) {
-        tape.plan(1);
-        var schema = t.struct({
-            policy:t.enums.of("option1 option2 option3")
-        });
-        tape.strictEqual(
-            new Form({
-                options:sample.options,
-                type:schema,
-                value:sample.value
-            }),
-        'option2', 'should handle option from json');
+if (typeof window !== 'undefined') {
 
+
+    tape.only('Form', function (tape) {
+        console.log('testttt')
+        tape.test('value', function (tape) {
+            tape.plan(1);
+
+            var sample = JSON.parse('{ "options": { "fields":{ "policy":{ "options":[ {"value":"option1", "text":"option 1"}, {"value":"option2", "text":"option 2"}, {"value":"option3", "text":"option 3"} ] } } }, "value": { "policy": "option2" } }');
+
+            var schema = t.struct({
+                policy: t.enums.of("option1 option2 option3")
+            });
+            tape.strictEqual(
+                new Form({
+                    options: sample.options,
+                    type: schema,
+                    value: sample.value
+                }),
+                'option2', 'should handle option from json');
+
+        });
     });
-});
+}
